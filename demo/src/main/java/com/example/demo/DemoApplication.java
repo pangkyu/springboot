f8848a885f9107d7b92edfbdf7cbe7d0ced76369
bae-seongkyu <75983289+pangkyu@users.noreply.github.com>
2022-01-05 01:01:16 +0900

    0105

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,22 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+
+		ApplicationContext applicationContext = SpringApplication.run(DemoApplication.class, args);
+		String[] beanName = applicationContext.getBeanDefinitionNames();
+		Arrays.sort(beanName);
+		for(String b : beanName){
+			System.out.println(b);
+		}
 	}
 
 }
